ec0900cb44f9b3b745a122a2ade9b68a145ed12d
Kyle Frank <kyle.frank@mail.wvu.edu>
2014-07-07 16:05:13 -0400

    removing old rake file

--- db/migrate/20140626203259_change_date_format_in_operations_checks.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDateFormatInOperationsChecks < ActiveRecord::Migration
-  def up
-    change_column :operations_checks, :date, :datetime
-  end
-
-  def down
-    change_column :operations_checks, :date, :date
-  end
-end
